2e3a340cc40673e77a72f120fc86f7ab3d6065ba
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-07-11 10:41:55 -0400

    create table to display order in show.html

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,4 +1,12 @@
 class ItemOrder < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  def subtotal
+    self.quantity * self.price_given
+  end
+
+  def item
+    Item.find(self.item_id)
+  end
 end
