0245f3c7dadddb2acebec5dabc05f8a7eff592a5
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-07-14 12:11:06 -0400

    created shared_partials folder in views

--- app/helpers/hello_world_helper.rb
+++ app/helpers/hello_world_helper.rb
@@ -2,7 +2,7 @@ module HelloWorldHelper
 
   def display_user_sign_in(user_signed_in)
     if user_signed_in
-      render ('user_signed_in')
+      render 'shared_partials/user_signed_in'
     else
       content_tag(:div, link_to("Login", new_user_session_path))
     end
