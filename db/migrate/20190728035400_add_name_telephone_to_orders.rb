11ae0b48cad0efa74394dfadbcc5f083d3df9f11
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-07-28 00:04:44 -0400

    added columns name and telephone to orders

---
+++ db/migrate/20190728035400_add_name_telephone_to_orders.rb
@@ -0,0 +1,6 @@
+class AddNameTelephoneToOrders < ActiveRecord::Migration[5.2]
+  def change
+    add_column :orders, :name, :string
+    add_column :orders, :telephone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_033344) do
+ActiveRecord::Schema.define(version: 2019_07_28_035400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 2019_07_25_033344) do
     t.decimal "cash_payed", default: "0.0"
     t.decimal "check_payed", default: "0.0"
     t.decimal "debit_payed", default: "0.0"
+    t.string "name"
+    t.string "telephone"
   end
 
   create_table "users", force: :cascade do |t|
