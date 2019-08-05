957cd2facce80d0ef4ac22f3827aa0d6a0288788
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-08-05 12:20:35 -0400

    separeated bundles and created component Accountant(not being displayed yet)

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,10 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
-import Orders from '../bundles/HelloWorld/components/Orders';
-import Order from '../bundles/HelloWorld/components/Order';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  HelloWorld, Orders, Order
-});
+ReactOnRails.register({ HelloWorld });
