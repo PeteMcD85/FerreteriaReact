338a24bbe9655fcf0c529a5699b215372ede4b82
Anibal Rodriguez <centrogabinetesanibal@gmail.com>
2019-08-06 09:05:29 -0400

    fixed reload problem caused by order_errors

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -214,14 +214,15 @@ export default class HelloWorld extends React.Component {
       }).then((res) => {
         let orderId = res.order_id,
             orderIdDiv = document.getElementById('order-id'),
-            creationErrors = res.creation_errors;
+            orderErrors = res.order_errors;
         orderIdDiv.innerText = `Order Number : ${orderId}`;
         window.print();
         printButton.disabled = false
         printButton.innerHTML = "Imprima el Recibo"
         // location.reload();
+        console.log('res')
         console.log(res);
-        if(creationErrors.length > 0) return alert('Todo los articulos no fui en el Orden, save un copy de reciept y llama Stephen. Por Favor Reload Page')
+        if(orderErrors.length > 0) return alert('Todo los articulos no fui en el Orden, save un copy de reciept y llama Stephen. Por Favor Reload Page')
         location.reload(true);
 
         // this.updateSelectedNavList("Todo");
