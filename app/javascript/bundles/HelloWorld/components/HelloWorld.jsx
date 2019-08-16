04fdead7eff346057e108474e4a5d4f74bd6adde
Anibal <centrogabinetesanibal@gmail.com>
2019-08-16 10:11:15 -0400

    fixing styles errors after pulling

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -466,71 +466,6 @@ export default class HelloWorld extends React.Component {
              }
              { !showAccountant &&
                <div>
-                 <div id="order-id"></div>
-                 <div id="order-name-div">
-                  <label>
-                    <input id="order-name" placeholder="Nombre" />
-                  </label>
-                  <label>
-                    <input id="order-phone"  placeholder="Telefono" />
-                  </label>
-                 </div>
-                 <div className="payment-methods">
-                   <label>Libre De Impuestos
-                     <input type='checkbox' id="tax-free" onChange={this.updateTaxFree}/>
-                   </label>
-                   <label>Efectivo
-                     <input type='radio' name="paymentMethod" value="cash" onChange={this.updatePaymentMethod}/>
-                   </label>
-                   <label>Tarjeta De Crédito
-                     <input type='radio' name="paymentMethod" value="creditCard" onChange={this.updatePaymentMethod}/>
-                   </label>
-                   <label>Cheque
-                     <input type='radio' name="paymentMethod" value="check" onChange={this.updatePaymentMethod}/>
-                   </label>
-                   <label>Débito
-                     <input type='radio' name="paymentMethod" value="debit" onChange={this.updatePaymentMethod}/>
-                   </label>
-                   <span>
-                     <label>Personalizado
-                       <input type='radio' name="paymentMethod"  value="custom" onChange={this.updatePaymentMethod}/>
-                     </label>
-                     <div id="custom-payment-method-div" className="hidden">
-                      <div id="custom-payment-method">
-                         <label>Efectivo
-                           <input type='number' id="custom-cash" onChange={this.updateCustomInputChange} />
-                         </label>
-                         <label>Tarjeta De Crédito
-                           <input type='number' id="custom-credit-card" onChange={this.updateCustomInputChange} />
-                         </label>
-                         <label>Cheque
-                           <input type='number' id="custom-check" onChange={this.updateCustomInputChange} />
-                         </label>
-                         <label>Débito
-                           <input type='number' id="custom-debit" onChange={this.updateCustomInputChange} />
-                         </label>
-                       </div>
-                       <div id="custom-change" >
-                        {`${(customerChange< 0) ? 'Falta' : 'Cambio de Cliente' } : $ ${Math.abs(customerChange).toFixed(2)}`}
-                       </div>
-                      </div>
-                       <div id="cash-payment-method" className="hidden">
-                       <label>Efectivo Recibido
-                         <input type='number' id="cash-recieved" onChange={this.updateCashRecieved}/>
-                       </label>
-                       <span>  {`${(customerChange< 0) ? 'Falta' : 'Cambio de Cliente' } : $ ${Math.abs(customerChange).toFixed(2)}`}</span>
-                    </div>
-                   </span>
-                 </div>
-                 <Cart
-                   cart={cart}
-                   removeFromCart={this.removeFromCart}
-                   updateCartItem={this.updateCartItem}
-                   orderCart={this.orderCart}
-                   addCustomItemToCart={this.addCustomItemToCart}
-                 />
-               </div>
-             }
                  <div>
                    <div className="cart-buttons">
                      <button id="cart-button" onClick={this.cartButton}>
@@ -637,7 +572,6 @@ export default class HelloWorld extends React.Component {
                 columnName="category"
                 getCategoryBrand={this.getCategoryBrand}
              />
-
            </div>
            {showQueryList &&
             <div>
@@ -700,7 +634,11 @@ export default class HelloWorld extends React.Component {
      }
                </div>
              }
+
            </div>
+         }
+
+       </div>
     );
   } // END of render
 } // END of class
