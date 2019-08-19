c13ba955efc7185accdb86edf5bcfddcb06cf00f
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-08-19 11:56:34 -0400

    Created Partial for order Details and added it to edit_order_path

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,6 +37,9 @@ protect_from_forgery :except => [:create]
 
   def edit
     @order = Order.find(params[:id])
+    @cart = @order.item_orders
+    @cart_item_orders = @cart.map {|cart_item| Item.find(cart_item.item_id) }
+    @cart_custom_items = @order.custom_items.map {|cart_item| CustomItem.find(cart_item.id) }
   end
 
   def create
