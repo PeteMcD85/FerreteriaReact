eb2326ec608397d43f5242f8653dfe945706607e
Anibal <centrogabinetesanibal@gmail.com>
2019-08-19 11:23:47 -0400

    fixed styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # SSM
-Rails.application.config.assets.precompile += %w( print.css )
+Rails.application.config.assets.precompile += %w( print.scss )
+Rails.application.config.assets.precompile += %w( hello_world.scss )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
