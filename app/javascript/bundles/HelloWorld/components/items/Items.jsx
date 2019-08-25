73bba020e39d40656bd2037dc44cda239a2dda52
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-08-25 10:09:40 -0400

    created an array of brands and category that should be displayed in a table in Items.jsx

--- app/javascript/bundles/HelloWorld/components/items/Items.jsx
+++ app/javascript/bundles/HelloWorld/components/items/Items.jsx
@@ -14,30 +14,13 @@ const Items = props => {
     removeFromCart = props.removeFromCart,
     itemsStartRange = props.itemsStartRange,
     itemsEndRange = props.itemsEndRange,
-    updateItemsRange = props.updateItemsRange;
-  if (
-    selectedNavName !== "PVC" &&
-    selectedNavName !== "Tornillos" &&
-    selectedNavName !== "Tinte" &&
-    selectedNavName !== "query" &&
-    selectedNavName !== "Gozne" &&
-    selectedNavName !== "Correderas" &&
-    selectedNavName !== "Routers" &&
-    selectedNavName !== "Tapcon" &&
-    selectedNavName !== "Staples" &&
-    selectedNavName !== "Laminados" &&
-    selectedNavName !== "Sait" &&
-    selectedNavName !== "SeamFil" &&
-    selectedNavName !== "Clavos" &&
-    selectedNavName !== "Discos" &&
-    selectedNavName !== "Fregaderos" &&
-    selectedNavName !== "Superficie" &&
-    selectedNavName !== "Brazos" &&
-    selectedNavName !== "Madera" &&
-    selectedNavName !== "Tiradores" &&
-    selectedNavName !== "Lazy Susan" &&
-    selectedNavName !== "Temar"
-  ) {
+    updateItemsRange = props.updateItemsRange,
+    categoryBrandTurnedTable = ["PVC", "Tornillos", "Tinte", "query",
+     "Gozne", "Correderas", "Routers", "Tapcon", "Staples", "Laminados", "Sait",
+      "SeamFil", "Clavos", "Discos", "Fregaderos", "Superficie", "Brazos",
+    "Madera", "Tiradores", "Lazy Susan", "Temar"];
+
+  if ( !categoryBrandTurnedTable.includes(selectedNavName) ) {
     return (
       <div className="item-cards">
         <ItemsCard
@@ -59,36 +42,12 @@ const Items = props => {
       itemsTable = [],
       showTable = false;
     items.forEach(item => {
-      if (
-        item.category === "PVC" ||
-        item.category === "Tornillos" ||
-        item.category === "Tinte" ||
-        item.category === "Gozne" ||
-        item.category === "Correderas" ||
-        item.category === "Routers" ||
-        item.category === "Tapcon" ||
-        item.category === "Staples" ||
-        item.category === "Laminados" ||
-        item.brand === "Sait" ||
-        item.category === "SeamFil" ||
-        item.category === "Clavos" ||
-        item.category === "Discos" ||
-        item.category === "Superficie" ||
-        item.category === "Fregaderos" ||
-        item.category === "Temar" ||
-        item.category === "Brazos" ||
-        item.category === "Tiradores" ||
-        item.category === "Madera" ||
-        item.category === "Lazy Susan"
-      ) {
+      if (categoryBrandTurnedTable.includes(item.category) || item.brand === "Sait") {
         itemsTable.push(item);
       } else {
         itemsCard.push(item);
       }
     });
-    console.log(items);
-    console.log("itemsCard");
-    console.log(itemsCard);
     if (itemsTable.length > 0) showTable = true;
     return (
       <div>
