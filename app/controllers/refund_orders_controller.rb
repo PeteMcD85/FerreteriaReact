b52a1f45e099bda04662c74811b9266f951d1496
Stephen McDonald <stephenmcdonald8787@gmail.com>
2019-09-19 17:13:14 -0400

    displaying order in RefundOrder component

--- app/controllers/refund_orders_controller.rb
+++ app/controllers/refund_orders_controller.rb
@@ -2,16 +2,16 @@ class RefundOrdersController < ApplicationController
   layout 'orders'
 
   def new
-    p '========================'
-    p params
     @order = Order.find(params[:order_id])
-    @cart = @order.item_orders
-    @cart_item_orders = @cart.map {|cart_item| Item.find(cart_item.item_id) }
-    @cart_custom_items = @order.custom_items.map {|cart_item| CustomItem.find(cart_item.id) }
+    @item_orders = @order.item_orders.map { |io|
+      item = Item.find(io.item_id).as_json.except!(:id)
+      io.as_json.merge!(item: Item.find(io.item_id).as_json)
+    }
+    @custom_items = @order.custom_items
   end
 
   def create
-    
+
   end
 
 end
--- app/javascript/bundles/Orders/components/RefundOrder.jsx
+++ app/javascript/bundles/Orders/components/RefundOrder.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 
 export default class RefundOrder extends React.Component {
   static propTypes = {
-    order: PropTypes.object.isRequired
+    order: PropTypes.object.isRequired,
+    custom_items: PropTypes.array.isRequired,
+    item_orders: PropTypes.array.isRequired
   };
   /**
    * @param props
@@ -12,7 +14,9 @@ export default class RefundOrder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      order: props.order
+      order: props.order,
+      customItems: props.custom_items,
+      itemOrders: props.item_orders
     };
   }
 
@@ -20,14 +24,59 @@ export default class RefundOrder extends React.Component {
   }
 
   render() {
+
+    let order = this.state.order,
+      itemOrders = this.state.itemOrders,
+      customItems = this.state.customItems;
     console.log(this.state);
     return (
       <div className="refund-order">
-
         <table>
           <caption>Refund Order</caption>
           <tbody>
-
+            <tr>
+              <th>Marca</th>
+              <th>Nombre</th>
+              <th>Color</th>
+              <th>Tamaño</th>
+              <th>Grosor</th>
+              <th>Precio Dado</th>
+              <th>Cantidad</th>
+              <th>Total Parcial</th>
+              <th>Cantidad Reembolsada</th>
+              <th>Subtotal De Reembolso</th>
+            </tr>
+            {itemOrders.map((itemOrder, ind)=>{
+              return (
+                <tr key={ind} className="item-order">
+                  <td>{ itemOrder.item.brand }</td>
+                  <td>{ itemOrder.item.name }</td>
+                  <td>{ itemOrder.item.color }</td>
+                  <td>{ itemOrder.item.size }</td>
+                  <td>{ itemOrder.item.thickness }</td>
+                  <td>{ itemOrder.price_given}</td>
+                  <td>{ itemOrder.quantity }</td>
+                  <td>{ itemOrder.subtotal }</td>
+                  <td>{ itemOrder.subtotal_refunded }</td>
+                  <td>{ itemOrder.quantity_refunded }</td>
+                </tr>
+              )
+            })}
+            {customItems.map((customItem, ind)=>{
+              return (
+                <tr key={ind} className="custom-item">
+                  <td></td>
+                  <td>{customItem.name} </td>
+                  <td></td><td></td><td></td>
+                  <td>${customItem.price_given}</td>
+                  <td>{customItem.quantity} </td>
+                  <td>${customItem.subtotal} </td>
+                  <td>-${customItem.subtotal_refunded} </td>
+                  <td>{customItem.quantity_refunded} </td>
+                  <td></td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
